807322b92ea4e7f4adf1808ede40b6ad9f7e24ca
kuczumulunga <kuczumulunga123@wp.pl>
2022-06-03 21:02:37 +0200

    ~commit początek rejestracji

---
+++ src/main/java/pl/sda/finalproject/travelagency/module/register/RegisterMapping.java
@@ -0,0 +1,18 @@
+package pl.sda.finalproject.travelagency.module.register;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class RegisterMapping {
+
+    @PostMapping("/login")
+    public String list(Model model) {
+        return "/registration/registration.html";
+    }
+
+}
+

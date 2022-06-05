e5737563fb4c420f77c0535b56c2f7ea5beb3a2d
kuczumulunga <kuczumulunga123@wp.pl>
2022-06-05 15:31:42 +0200

    ~commit re

--- src/main/java/pl/sda/finalproject/travelagency/security/SecSecurityConfig.java
+++ src/main/java/pl/sda/finalproject/travelagency/security/SecSecurityConfig.java
@@ -1,39 +1,55 @@
 package pl.sda.finalproject.travelagency.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-    }
-    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin()
-                .loginPage("/login.html")
-                .defaultSuccessUrl("/index.html",true)
-                .failureUrl("/login.html?error=true");
+
                 http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/user/**", "/index.html", "/trip").hasRole("USER")
-                .antMatchers("/admin/**", "/index.html", "/trip").hasRole("ADMIN")
+                .antMatchers("/user/**", "/trip").hasRole("USER")
+                .antMatchers("/admin/**", "/trip").hasRole("ADMIN")
                 .antMatchers("/anonymous*").anonymous()
-                .antMatchers("/login").permitAll()
-                .anyRequest().authenticated();
+                .antMatchers("/login", "/index.html").permitAll()
+                        .antMatchers("/templates/**","/static/**", "/registration/**").permitAll()
 
-
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .deleteCookies("JSESSIONID");
+        http.formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/index.html",true)
+                .failureUrl("/login?error=true");
 
 
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1").password(passwordEncoder().encode("user1Pass")).roles("USER")
+                .and()
+                .withUser("user2").password(passwordEncoder().encode("user2Pass")).roles("USER")
+                .and()
+                .withUser("admin").password(passwordEncoder().encode("123")).roles("ADMIN");
+    }
 
     public SecSecurityConfig() {
         super();
